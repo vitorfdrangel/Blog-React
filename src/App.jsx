7973626b5bf12d04c6f9009565e3143035cfe4cd
Vitor Rangel <vitorfdrangel@gmail.com>
2024-07-02 19:15:21 -0300

    Blog-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
       <div className="Container">
+        <Navbar />
         <Outlet />
       </div>
     </div>
