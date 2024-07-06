c4339cbbf3192747f1b61a79c2d017f7ab473b10
Vitor Rangel <vitorfdrangel@gmail.com>
2024-07-05 21:09:53 -0300

    Blog-React

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,20 +1,51 @@
+import BlogFetch from "../axios/config";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./NewPost.css";
 
 const NewPost = () => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const post = { title, body, id: 1 };
+
+    BlogFetch.post("/posts", {
+      body: post,
+    });
+
+    navigate("/");
+  };
+
   return (
     <div className="new-post">
       <h2>Inserir novo Post:</h2>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div className="form-control">
           <label>
             <span>Título:</span>
-            <input type="text" id="title" placeholder="Digite o título" />
+            <input
+              type="text"
+              id="title"
+              placeholder="Digite o título"
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </label>
         </div>
         <div className="form-control">
           <label>
             <span>Conteúdo:</span>
-            <textarea id="body" placeholder="Digite o conteúdo"></textarea>
+            <textarea
+              id="body"
+              placeholder="Digite o conteúdo"
+              onChange={(e) => setBody(e.target.value)}
+            ></textarea>
           </label>
         </div>
         <input type="submit" value="Criar Post" className="btn" />
